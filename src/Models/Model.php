8c3d9bfbf671efbba9c9c363aba22f74db0805e2
ValentinGlairot <valentin.glairot@viacesi.fr>
2025-03-20 14:45:29 +0100

    Delete src directory

--- src/Models/Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-
-/**
- * The Model class is an abstract class that serves as the base class for all models in the application.
- */
-abstract class Model {
-    protected $connection = null;
-}
