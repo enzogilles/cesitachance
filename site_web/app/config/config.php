197b1d34ffcf9636b1a3e7c2bf3cecfa71edbbfb
ValentinGlairot <valentin.glairot@viacesi.fr>
2025-03-20 16:21:23 +0100

    Delete site_web directory

--- site_web/app/config/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-// app/config/config.php
-define('BASE_URL', 'http://localhost/site_web/public/');
--- site_web/app/views/utilisateurs/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Ce fichier n'est pas destiné à être affiché directement.
-// La déconnexion est gérée via le contrôleur et ce fichier redirige vers la page de connexion.
-session_start();
-session_destroy();
-header("Location: " . BASE_URL . "utilisateurs/connexion.php");
-exit;
