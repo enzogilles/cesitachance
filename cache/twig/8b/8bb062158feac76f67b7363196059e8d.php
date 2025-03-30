789df0a00272c8b0cb19f3fa7d856ac5d3a5c197
RockWeb250 <zoe.truongcanh@viacesi.fr>
2025-03-30 19:53:10 +0200

    correction suppression wishlist

--- cache/twig/8b/8bb062158feac76f67b7363196059e8d.php
+++ cache/twig/8b/8bb062158feac76f67b7363196059e8d.php
@@ -232,6 +232,21 @@ class __TwigTemplate_79e17c253dd3b7743ff30cfc98e885c4 extends Template
         yield "
   
 </main>
+<script>
+  const BASE_URL = \"";
+        // line 74
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["BASE_URL"] ?? null), "html", null, true);
+        yield "\";
+</script>
+
+<script src=\"";
+        // line 77
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["BASE_URL"] ?? null), "html", null, true);
+        yield "public/js/wishlist.js\"></script>
+<script src=\"";
+        // line 78
+        yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(($context["BASE_URL"] ?? null), "html", null, true);
+        yield "public/js/gerer-offres.js\"></script>
 ";
         return; yield '';
     }
@@ -257,7 +272,7 @@ class __TwigTemplate_79e17c253dd3b7743ff30cfc98e885c4 extends Template
      */
     public function getDebugInfo()
     {
-        return array (  232 => 70,  226 => 67,  222 => 65,  218 => 63,  214 => 61,  206 => 58,  202 => 56,  200 => 55,  196 => 54,  192 => 53,  186 => 52,  183 => 51,  179 => 50,  176 => 49,  174 => 48,  167 => 47,  164 => 46,  161 => 44,  157 => 42,  153 => 40,  139 => 34,  132 => 32,  129 => 31,  125 => 30,  122 => 29,  120 => 28,  117 => 27,  114 => 26,  111 => 24,  107 => 22,  103 => 20,  92 => 15,  88 => 14,  82 => 13,  79 => 12,  75 => 11,  72 => 10,  70 => 9,  67 => 8,  64 => 7,  60 => 4,  56 => 3,  48 => 2,  37 => 1,);
+        return array (  248 => 78,  244 => 77,  238 => 74,  232 => 70,  226 => 67,  222 => 65,  218 => 63,  214 => 61,  206 => 58,  202 => 56,  200 => 55,  196 => 54,  192 => 53,  186 => 52,  183 => 51,  179 => 50,  176 => 49,  174 => 48,  167 => 47,  164 => 46,  161 => 44,  157 => 42,  153 => 40,  139 => 34,  132 => 32,  129 => 31,  125 => 30,  122 => 29,  120 => 28,  117 => 27,  114 => 26,  111 => 24,  107 => 22,  103 => 20,  92 => 15,  88 => 14,  82 => 13,  79 => 12,  75 => 11,  72 => 10,  70 => 9,  67 => 8,  64 => 7,  60 => 4,  56 => 3,  48 => 2,  37 => 1,);
     }
 
     public function getSourceContext()
@@ -334,6 +349,12 @@ class __TwigTemplate_79e17c253dd3b7743ff30cfc98e885c4 extends Template
 
   
 </main>
+<script>
+  const BASE_URL = \"{{ BASE_URL }}\";
+</script>
+
+<script src=\"{{ BASE_URL }}public/js/wishlist.js\"></script>
+<script src=\"{{ BASE_URL }}public/js/gerer-offres.js\"></script>
 {% endblock %}
 ", "wishlist/index.twig", "C:\\site_localhost\\cesitachance-3\\app\\views\\wishlist\\index.twig");
     }
